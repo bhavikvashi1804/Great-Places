4169ee003d254b0b133251c4411b3466437bdcce
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-03-24 15:39:43 +0530

    image picker use image picker package to gert image

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   @override
@@ -10,6 +11,17 @@ class ImageInput extends StatefulWidget {
 class _ImageInputState extends State<ImageInput> {
 
   File _storedImage;
+
+
+  Future<void>_takePicture()async{
+    final imageFile=await ImagePicker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+
+    );
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -38,6 +50,7 @@ class _ImageInputState extends State<ImageInput> {
         Expanded(
           child: FlatButton.icon(
             onPressed: (){
+              _takePicture();
 
             }, 
             icon: Icon(Icons.camera),
